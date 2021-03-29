34022434db4074d580712db9fe5ed9dfc92ca149
davidcastel <davidccbp20@gmail.com>
2021-03-29 00:17:00 -0400

    Create individual file for each class for Issue #1

---
+++ library/SeeSpecificElements.py
@@ -0,0 +1,70 @@
+from CSVLibrary import *
+
+
+class SeeSpecificElements(CSVLibrary):
+    def __init__(self, file, seperator, dic, row=0, column=0):
+        super().__init__(file, seperator)
+        self.__dic = dic
+        self.__row = row
+        self.__col = column
+
+    # *** Getters ***
+    def getRow(self):
+        return self.__row
+
+    def getCol(self):
+        return self.__col
+
+    def getLocalDict(self):
+        return self.__dic
+
+    def setDict(self, newDict):
+        self.__dic = newDict
+
+    def see(self):
+        temp_dict = self.getLocalDict()
+
+        if self.__is_row_and_col_0():
+            self.getLocalDict()
+        elif self.__is_column_only_0():
+            self.__iterateOnlyRow(temp_dict)
+        elif self.__is_row_only_0():
+            self.__iterateOnlyCol(temp_dict)
+        else:
+            self.__iterateRowAndCol(temp_dict)
+
+    # Private Methods
+    def __iterateOnlyRow(self, temp_dict):
+        temp_row = self.getRow()
+
+        for key in temp_dict:
+            if key != temp_row:
+                temp_dict[key] = ""
+
+    def __iterateOnlyCol(self, temp_dict):
+        temp_col = self.getCol() - 1
+
+        for key in temp_dict:
+            temp_list = temp_dict[key]
+            for i in range(0, len(temp_list)):
+                if i != temp_col:
+                    temp_list[i] = ""
+
+    def __iterateRowAndCol(self, temp_dict):
+        temp_row = self.getRow()
+        temp_col = self.getCol() - 1
+
+        for key in temp_dict:
+            temp_list = temp_dict[key]
+            for i in range(0, len(temp_list)):
+                if i != temp_col and key != temp_row:
+                    temp_list[i] = ""
+
+    def __is_row_and_col_0(self):
+        return self.getRow() == 0 and self.getCol() == 0
+
+    def __is_column_only_0(self):
+        return self.getRow() != 0 and self.getCol() == 0
+
+    def __is_row_only_0(self):
+        return self.getRow() == 0 and self.getCol() != 0
